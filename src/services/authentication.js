24c4d40a77d3ba17b511c9d2c6e3f6f8ab016ef2
Daniel Carvalho <ti.danielcarvalho@gmail.com>
2022-09-21 14:31:44 -0300

    desmembrado algumas funcoes que nao eram de responsabilidades do router e trazido para um service

---
+++ src/services/authentication.js
@@ -0,0 +1,49 @@
+exports.errorLogin = (req, res) => {
+  console.log("Usuário não identificado!")
+  req.session.loginErro = true;
+  req.session.email = req.body.email;
+  res.redirect('/');
+}
+
+exports.login = async (db, email, password) => {
+    const sql = 'SELECT * FROM users WHERE password = ? AND email LIKE ?';
+    const objResponse = {
+        loginErr: false,
+        login: email,
+        name: ''
+    }
+    const result = await new Promise((resolve, reject) => {
+        db.query(sql, [password, email], function (err, results){
+            if (results && results.length > 0){
+                console.log("Login feito com sucesso!");
+                console.log("Usuário "+email+" identificado!")
+                objResponse.name = 'adicionar'
+                return resolve(objResponse);
+            }else{
+                console.log("Usuário não identificado!")
+                objResponse.loginErr = true
+                return resolve(objResponse);
+            }
+        })
+    });
+    return result
+}
+exports.userPermission = async (db, email) => {
+    const sql = 'SELECT * FROM users WHERE email = ? ';
+    const panelAgent = '/painel-agente'
+    const panelSupervisor = '/painel-supervisor'
+
+    const result = await new Promise((resolve, reject) => {
+        db.query(sql, [email], function (err, result) {
+            //PERMISSÃO 0 = AGENTE
+            if(result[0].permission == 0){
+                return resolve({panel: panelAgent, permission: 0})
+            }
+            //PERMISSÃO 1 = SUPERVISOR
+            if(result[0].permission == 1){
+                return resolve({panel: panelSupervisor, permission: 1})
+            }
+        });
+    });
+    return result
+}
